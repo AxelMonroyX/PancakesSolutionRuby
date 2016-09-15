c6542a9c878ab4462305b803ffd9726fcc57e5e1
Axel Monroy <xaxelmonroyx@gmail.com>
2016-09-15 06:28:37 -0700

    Array adapted for counters

--- lib/solution-infinite-house-pancakes.rb
+++ lib/solution-infinite-house-pancakes.rb
@@ -1,28 +1,39 @@
 num_cases = gets.to_i
 
 for i in 0..num_cases - 1
-    num_plates = gets.to_i
-    pancakes = Array.new(num_plates)
-    pancakes = gets.split(' ').map(&:to_i)
+  num_plates = gets.to_i
+  pancakes = Array.new(num_plates)
+  pancakes = gets.split(' ').map(&:to_i)
 
-    max_p = pancakes.max
-    counter = Array.new(max_p)
+  max_p = (pancakes.sort_by {|k,v| v}.reverse)[0].to_i
+  max_p += 1
+  puts "MAX: "+    max_p.to_s
+  counter = Array.new(max_p)
 
-    counter = pancakes.map { 1 }
+  counter = pancakes.map { 1 }
 
-    puts '--------------------------------------'
-moves = max_p;
-    min_minutes = 10_000
+  puts '--------------------------------------'
+  moves = max_p;
+  puts "MOVESS"+moves.to_s
+  min_minutes = 10_000
+  splits = 0
+  counter.each.with_index(1) do |_item_k, k|
+    # splits = 0
+    counter.each.with_index(1) do |_item_l, l|
 
-    counter.each.with_index(1) do |_item_k, k|
-        splits = 0
-        counter.each.with_index(1) do |_item_l, l|
-            splits += ((l - 1) / k) * _item_l;
-            puts 'Splits: ' + splits.to_s
-        end
-
-        moves = splits + k if splits + k <= moves
+      puts 'Splits: ' + splits.to_s
+      splits=  splits + (((l - 1) / k) * _item_l.to_i) 
+      puts 'Splits: ' + splits.to_s
+    end
+    puts "=========================================="
+    #  puts "KKKK" +k.to_s
+    #  puts "spplit: " + splits.to_s
+    if splits + k.to_i <= moves.to_i
+      moves = splits + k
     end
 
-    printf "Case #%d: %d\n", (i + 1), moves
+
+end
+
+printf "Case #%d: %d\n", (i + 1), moves+1
 end
