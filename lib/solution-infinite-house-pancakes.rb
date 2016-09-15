06f3aa537970f4ac0c349e36ca126481bbf6c40a
Axel Monroy <xaxelmonroyx@gmail.com>
2016-09-14 21:41:05 -0700

    Singles cases true

--- lib/solution-infinite-house-pancakes.rb
+++ lib/solution-infinite-house-pancakes.rb
@@ -1,30 +1,28 @@
 num_cases = gets.to_i
 
-for i in 0..num_cases-1
+for i in 0..num_cases - 1
     num_plates = gets.to_i
     pancakes = Array.new(num_plates)
     pancakes = gets.split(' ').map(&:to_i)
-    puts pancakes.inspect
-    printf 'pancakes taken'
 
-    confirm = Array.new(10_000)
+    max_p = pancakes.max
+    counter = Array.new(max_p)
 
-    confirm = pancakes.map { 1 }
-    puts confirm.inspect
+    counter = pancakes.map { 1 }
+
     puts '--------------------------------------'
-
+moves = max_p;
     min_minutes = 10_000
 
-    confirm.each.with_index(1) do |item_k, k|
+    counter.each.with_index(1) do |_item_k, k|
         splits = 0
-        confirm.each_with_index do |item_l, l|
-
-            splits += ((l - 1) / k) * confirm[l].to_i;
-            puts "Splits: " +splits.to_s
+        counter.each.with_index(1) do |_item_l, l|
+            splits += ((l - 1) / k) * _item_l;
+            puts 'Splits: ' + splits.to_s
         end
 
-        min_minutes = splits + k if (splits + k < min_minutes)
+        moves = splits + k if splits + k <= moves
     end
 
-    printf "Case #%d: %d\n", (i + 1), min_minutes
+    printf "Case #%d: %d\n", (i + 1), moves
 end
